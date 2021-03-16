13084c3a309d7fa8ead3874609e411ecad89a4ce
Eduardo Almeida <web2solucoes@gmail.com>
2021-03-16 01:03:32 -0300

    fix handlers

--- src/store/events/onCustomerDelete.js
+++ src/store/events/onCustomerDelete.js
@@ -1,6 +1,6 @@
 
 
-export default async function (eventObj) {
+export default  function (eventObj) {
   const { /* data,  foundation, */error } = eventObj
   if (error) {
     throw new Error(`Error deleting order: ${error}`)
